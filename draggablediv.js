61bc5e5b830d85e6ef7eba64c72b8fa492d49495
michaelnguyen19 <michaelnguyen202069@gmail.com>
2025-09-21 11:59:17 -0700

    draggable div finished

--- draggablediv.js
+++ draggablediv.js
@@ -9,33 +9,34 @@ function dragElement(elmnt) {
         //The DIV can be moved elsewhere
         elmnt.onmousedown = dragMouseDown;
     }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault(); //preventDefault prevents the predicted action from happening
+        //get the mouse cursor position at startup
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        //set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement(){
+        //stop moving when mouse button is released
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
-function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault(); //preventDefault prevents the predicted action from happening
-    //get the mouse cursor position at startup
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves
-    document.onmousemove = elementDrag;
-}
-
-function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    //set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-}
-
-function closeDragElement(){
-    //stop moving when mouse button is released
-    document.onmouseup = null;
-    document.onmousemove = null;
-}
